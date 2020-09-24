f6b13e94257b3d141c452cef41b93566130bf2ce
linux_china <libing.chen@gmail.com>
2020-09-24 00:23:57 -0700

    polishing

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 module.exports = {
     entry: './src/index.ts',
     mode: "development",
-    "target": "web",
+    target: "web",
     plugins: [
         new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({template: 'index.html'})
